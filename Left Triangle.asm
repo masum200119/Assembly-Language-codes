171447b7d77326250c0459cd25b4bb1742bcb262
Masum Billah <95751615+masum200119@users.noreply.github.com>
2024-05-29 13:53:19 +0600

    Update Left Triangle.asm

--- Left Triangle.asm
+++ Left Triangle.asm
@@ -1,34 +1,51 @@
 .MODEL SMALL
-.STACK 100H
+.STACK 100H 
+.DATA
+MSG DB 'ENTER NUMBER OF LINES $'
+VAR DB ?
+   
 .CODE 
 MAIN PROC
-    MOV BX,1  
-    MOV CX,5
+    MOV AX,@DATA
+    MOV DS,AX
     
-    L1:
-    PUSH CX
+    MOV AH,9
+    LEA DX,MSG
+    INT 21H
+     
+    MOV AH,1
+    INT 21H 
+    SUB AL,48
+    MOV VAR,AL
     
-    
-    MOV CX,BX  
-    MOV AH,2
-    MOV DL,'*'
-    
-   L2:
-    INT 21H      
-    
-    LOOP L2
     MOV AH,2
     MOV DL,10
     INT 21H
     MOV DL,13
     INT 21H
     
-    INC BX
+    MOV CX,0
+    MOV CL,VAR
+    MOV BL,1
     
-    POP CX
+    TOP:  
+    MOV CX,BX 
     
+    L1:
+    CMP BL,VAR
+    JG EXIT
+    MOV AH,2
+    MOV DL,'*'
+    INT 21H
     LOOP L1
     
+    INC BL
+    MOV AH,2
+    MOV DL,10
+    INT 21H
+    MOV DL,13
+    INT 21H
+    LOOP TOP
     
     
     EXIT:
